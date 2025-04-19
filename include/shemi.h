7530c4b282f863f9bc3da96a7a46e3d2a5553ed7
yehuthi <yehuthith@gmail.com>
2025-04-19 20:35:13 +0300

    Implement some Hebrew functions

---
+++ include/shemi.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <uchar.h>
+#include <stdbool.h>
+
+bool shemi_block_hebrew(char32_t c);
+bool shemi_block_hebrew_alphabet(char32_t c);
+bool shemi_block_hebrew_cantillation(char32_t c);
+char32_t shemi_hebrew_to_phoenician_unchecked(char32_t c);
+char32_t shemi_hebrew_to_phoenician(char32_t c);
+bool shemi_hebrew_sofit(char32_t c);
+char32_t shemi_hebrew_to_sofit(char32_t c);
+char32_t shemi_hebrew_to_not_sofit(char32_t c);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/shemi.c
@@ -0,0 +1,57 @@
+#include "shemi.h"
+#include <stdint.h>
+
+static const char32_t hebrew_to_phoenician_table[] = {
+	0x10900, 0x10901, 0x10902, 0x10903, 0x10904,
+	0x10905, 0x10906, 0x10907, 0x10908, 0x10909,
+	0x1090A, 0x1090A, /* kaf */
+	0x1090B,
+	0x1090C, 0x1090C, /* mem */
+	0x1090D, 0x1090D, /* nun */
+	0x1090E, 0x1090F,
+	0x10910, 0x10910, /* pe */
+	0x10911, 0x10911, /* sadi */
+	0x10912, 0x10913, 0x10914, 0x10915
+};
+
+bool shemi_block_hebrew(char32_t c) {
+	return 0x0590 <= c && c <= 0x05FF;
+}
+
+bool shemi_block_hebrew_alphabet(char32_t c) {
+	return 0x05D0 <= c && c <= 0x05EA;
+}
+
+bool shemi_block_hebrew_cantillation(char32_t c) {
+	return 0x0591 <= c && c <= 0x05AF;
+}
+
+char32_t shemi_hebrew_to_phoenician_unchecked(char32_t c) {
+	return hebrew_to_phoenician_table[c - 0x05DA];
+}
+
+char32_t shemi_hebrew_to_phoenician(char32_t c) {
+	return shemi_block_hebrew_alphabet(c) ? shemi_hebrew_to_phoenician_unchecked(c) : c;
+}
+
+bool shemi_hebrew_sofit(char32_t c) {
+	for (uint8_t i = 0; i < hebrew_sofiyot_count; i++)
+		if (hebrew_not_sofiyot[i] == c) return true;
+	return false;
+}
+
+static const char32_t hebrew_sofiyot[] = { 0x05DA, 0x05DD, 0x05DF, 0x05E3, 0x05E5 };
+static const char32_t hebrew_not_sofiyot[] = { 0x5DB, 0x05DE, 0x05E0, 0x05E4, 0x05E6 };
+const uint8_t hebrew_sofiyot_count = sizeof(hebrew_sofiyot) / sizeof(hebrew_sofiyot[0]);
+
+char32_t shemi_hebrew_to_sofit(char32_t c) {
+	for (uint8_t i = 0; i < hebrew_sofiyot_count; i++)
+		if (hebrew_not_sofiyot[i] == c) return c - 1;
+	return c;
+}
+
+char32_t shemi_hebrew_to_not_sofit(char32_t c) {
+	for (uint8_t i = 0; i < hebrew_sofiyot_count; i++)
+		if (hebrew_sofiyot[i] == c) return c + 1;
+	return c;
+}
