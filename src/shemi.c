d3b27d72635f8b750128b39444814d27df7af483
yehuthi <yehuthith@gmail.com>
2025-04-19 21:12:02 +0300

    Organize

--- src/shemi.c
+++ src/shemi.c
@@ -1,7 +1,7 @@
 #include "shemi.h"
 #include <stdint.h>
 
-static const char32_t hebrew_to_phoenician_table[] = {
+static const char32_t HEBREW_TO_PHOENICIAN_TABLE[] = {
 	0x10900, 0x10901, 0x10902, 0x10903, 0x10904,
 	0x10905, 0x10906, 0x10907, 0x10908, 0x10909,
 	0x1090A, 0x1090A, /* kaf */
@@ -14,76 +14,86 @@ static const char32_t hebrew_to_phoenician_table[] = {
 	0x10912, 0x10913, 0x10914, 0x10915
 };
 
+static bool char_in_range(char32_t low, char32_t high, char32_t value) {
+	return low <= value && value <= high;
+}
+
 bool shemi_block_hebrew(char32_t c) {
-	return 0x0590 <= c && c <= 0x05FF;
+	return char_in_range(0x0590, 0x05FF, c);
 }
 
 bool shemi_block_hebrew_alphabet(char32_t c) {
-	return 0x05D0 <= c && c <= 0x05EA;
+	return char_in_range(0x05D0, 0x05EA, c);
 }
 
 bool shemi_block_hebrew_cantillation(char32_t c) {
-	return 0x0591 <= c && c <= 0x05AF;
+	return char_in_range(0x0591, 0x05AF, c);
 }
 
 bool shemi_block_phoenician(char32_t c) {
-	return 0x10900 <= c && c <= 0x1091F;
+	return char_in_range(0x10900, 0x1091F, c);
 }
 
 bool shemi_block_phoenician_alphabet(char32_t c) {
-	return 0x10900 <= c && c <= 0x10915;
+	return char_in_range(0x10900, 0x10915, c);
 }
 
 bool shemi_block_phoenician_number(char32_t c) {
-	return 0x10916 <= c && c <= 0x1091B;
+	return char_in_range(0x10916, 0x1091B, c);
 }
 
 bool shemi_block_aramaic(char32_t c) {
-	return 0x10840 <= c && c <= 0x1085F;
+	return char_in_range(0x10840, 0x1085F, c);
 }
 
 bool shemi_block_aramaic_alphabet(char32_t c) {
-	return 0x10840 <= c && c <= 0x10855;
+	return char_in_range(0x10840, 0x10855, c);
 }
 
 bool shemi_block_aramaic_number(char32_t c) {
-	return 0x10858 <= c && c <= 0x1085F;
+	return char_in_range(0x10858, 0x1085F, c);
 }
 
 bool shemi_block_samaritan(char32_t c) {
-	return 0x0800 <= c && c <= 0x083F;
+	return char_in_range(0x0800, 0x083F, c);
 }
 
 bool shemi_block_samaritan_alphabet(char32_t c) {
-	return 0x0800 <= c && c <= 0x0815;
+	return char_in_range(0x0800, 0x0815, c);
 }
 
 char32_t shemi_hebrew_to_phoenician_unchecked(char32_t c) {
-	return hebrew_to_phoenician_table[c - 0x05DA];
+	return HEBREW_TO_PHOENICIAN_TABLE[c - 0x05DA];
 }
 
 char32_t shemi_hebrew_to_phoenician(char32_t c) {
-	return shemi_block_hebrew_alphabet(c) ? shemi_hebrew_to_phoenician_unchecked(c) : c;
+	return shemi_block_hebrew_alphabet(c) ?
+		shemi_hebrew_to_phoenician_unchecked(c) : c;
 }
 
 bool shemi_hebrew_sofit(char32_t c) {
-	for (uint8_t i = 0; i < hebrew_sofiyot_count; i++)
-		if (hebrew_not_sofiyot[i] == c) return true;
+	for (uint8_t i = 0; i < HEBREW_SOFIYOT_COUNT; i++)
+		if (HEBREW_NOT_SOFIYOT[i] == c) return true;
 	return false;
 }
 
-static const char32_t hebrew_sofiyot[] = { 0x05DA, 0x05DD, 0x05DF, 0x05E3, 0x05E5 };
-static const char32_t hebrew_not_sofiyot[] = { 0x5DB, 0x05DE, 0x05E0, 0x05E4, 0x05E6 };
-const uint8_t hebrew_sofiyot_count = sizeof(hebrew_sofiyot) / sizeof(hebrew_sofiyot[0]);
+static const char32_t HEBREW_SOFIYOT[] = {
+	0x05DA, 0x05DD, 0x05DF, 0x05E3, 0x05E5
+};
+static const char32_t HEBREW_NOT_SOFIYOT[] = {
+	0x05DB, 0x05DE, 0x05E0, 0x05E4, 0x05E6
+};
+const uint8_t HEBREW_SOFIYOT_COUNT =
+	sizeof(HEBREW_SOFIYOT) / sizeof(HEBREW_SOFIYOT[0]);
 
 char32_t shemi_hebrew_to_sofit(char32_t c) {
-	for (uint8_t i = 0; i < hebrew_sofiyot_count; i++)
-		if (hebrew_not_sofiyot[i] == c) return c - 1;
+	for (uint8_t i = 0; i < HEBREW_SOFIYOT_COUNT; i++)
+		if (HEBREW_NOT_SOFIYOT[i] == c) return c - 1;
 	return c;
 }
 
 char32_t shemi_hebrew_to_not_sofit(char32_t c) {
-	for (uint8_t i = 0; i < hebrew_sofiyot_count; i++)
-		if (hebrew_sofiyot[i] == c) return c + 1;
+	for (uint8_t i = 0; i < HEBREW_SOFIYOT_COUNT; i++)
+		if (HEBREW_SOFIYOT[i] == c) return c + 1;
 	return c;
 }
