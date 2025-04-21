7562abe06b638c4d6a3c5b5f58419525ad46a08d
yehuthi <yehuthith@gmail.com>
2025-04-21 10:37:27 +0300

    Add benchmark for Phoenician inter convert

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -10,4 +10,16 @@ static void BM_hebrew_convert(benchmark::State &state) {
 	}
 }
 
+static void BM_phoenician_convert_inter(benchmark::State &state) {
+	for (auto _ : state) {
+		uint32_t c = 0x10915;
+		benchmark::DoNotOptimize(c);
+		benchmark::ClobberMemory();
+		benchmark::DoNotOptimize(
+			shemi_phoenician_convert_inter(c, SHEMI_PHNX, SHEMI_ARMI)
+		);
+	}
+}
+
 BENCHMARK(BM_hebrew_convert);
+BENCHMARK(BM_phoenician_convert_inter);
